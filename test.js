8f4feaa5332a63301a5c054520548369e08b877d
Yifei Wang <yiw085@ucsd.edu>
2023-03-15 11:37:25 -0700

    part2 finished

--- test.js
+++ test.js
@@ -3,10 +3,10 @@ let put = document.getElementById('put');
 let post = document.getElementById('post');
 let delete_but = document.getElementById('delete');
 
-get.addEventListener('click', getData());
-put.addEventListener('click', putData());
-post.addEventListener('click', postData());
-delete_but.addEventListener('click', deleteData());
+get.addEventListener('click', getData);
+put.addEventListener('click', putData);
+post.addEventListener('click', postData);
+delete_but.addEventListener('click', deleteData);
 
 function postData()
 {
@@ -17,14 +17,16 @@ function postData()
 
     let data = {'num': num.value, 'name': name.value, 'content': content.value, 'date': date.toString()};
     let url = 'https://httpbin.org/post';
-    let op = {method:'POST', body: JSON.stringify(data), headers: {'Content-Type': 'appliaction/JSON'}};
+    let op = {method:'POST', body: JSON.parse(data), headers: {'Content-Type': 'appliaction/JSON'}};
     fetch(url, op);
 }
 
 function getData()
 {
     let res = document.getElementById('response');
-    fetch('https://httpbin.org/get').then(response => response.json()).then(data => {
+    let url = 'https://httpbin.org/get';
+    let op = {method: 'GET'};
+    fetch(url, op).then(response => response.json()).then(data => {
         alert(JSON.stringify(data));
         res.innerHTML = JSON.stringify(data);
     });
@@ -39,12 +41,13 @@ function putData()
 
     let data = {'num': num.value, 'name': name.value, 'content': content.value, 'date': date.toString()};
     let url = 'https://httpbin.org/put';
-    let op = {method:'PUT', body: JSON.stringify(data), headers: {'Content-Type': 'appliaction/JSON'}};
+    let op = {method:'PUT', body: JSON.parse(data), headers: {'Content-Type': 'appliaction/JSON'}};
     fetch(url, op);
 }
 
 function deleteData()
 {
     let url = 'https://httpbin.org/delete';
-    fetch(url, {method: 'DELETE'});
+    let op = {method: 'DELETE'};
+    fetch(url, op);
 }
